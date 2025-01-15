b0ab62003ac2998d4f8e0cca0ebbef7f59f5561f
melserngl <128845117+nglmercer@users.noreply.github.com>
2025-01-14 22:19:52 -0500

    add new component
    add component to managmente mods and plugins

--- web/js/classes/KubekRefresher.js
+++ web/js/classes/KubekRefresher.js
@@ -107,3 +107,123 @@ class KubekRefresher {
         }, "tasksList");
     }
 }
+class KubekRefresherComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.refreshIntervals = {};
+        this.isItFirstLogRefresh = false;
+        this.previousConsoleUpdateLength = 0;
+        this.timeStampRegexp = /\[[0-9]{2}\:[0-9]{2}\:[0-9]{2}\]/gm;
+    }
+
+    // Método para añadir un intervalo de refresco
+    addRefreshInterval(interval, handler, name) {
+        this.refreshIntervals[name] = setInterval(handler, interval);
+    }
+
+    // Método para eliminar un intervalo de refresco
+    removeRefreshInterval(name) {
+        clearInterval(this.refreshIntervals[name]);
+    }
+
+    // Método para añadir el intervalo de refresco del encabezado del servidor
+    addRefreshServerHeaderInterval() {
+        this.addRefreshInterval(1500, () => {
+            KubekServerHeaderUI.refreshServerHeader(() => {});
+        }, "serverHeader");
+    }
+
+    // Método para añadir el intervalo de refresco del log del servidor
+    addRefreshServerLogInterval() {
+        this.addRefreshInterval(650, () => {
+            this.refreshConsoleLog();
+        }, "serverConsole");
+    }
+
+    // Método para añadir el intervalo de refresco del uso de recursos
+    addRefreshUsageInterval() {
+        this.addRefreshInterval(5000, () => {
+            if (typeof KubekConsoleUI !== "undefined") {
+                KubekHardware.getUsage((usage) => {
+                    KubekConsoleUI.refreshUsageItems(usage.cpu, usage.ram.percent, usage.ram);
+                });
+            }
+        }, "usage");
+    }
+
+    // Método para refrescar el log de la consola
+    refreshConsoleLog() {
+        let consoleTextElem = this.querySelector("#console-text");
+        if (consoleTextElem) {
+            KubekServers.getServerLog(selectedServer, (serverLog) => {
+                let parsedServerLog = serverLog.split(/\r?\n/);
+                if (this.previousConsoleUpdateLength === serverLog.length) {
+                    return;
+                }
+                this.previousConsoleUpdateLength = serverLog.length;
+                consoleTextElem.innerHTML = "";
+                parsedServerLog.forEach((line) => {
+                    let html_text = "";
+                    let parsedText = ANSIParse(KubekUtils.linkify(mineParse(line).raw));
+                    if (parsedText.length > 1) {
+                        let joinedLine = "";
+                        parsedText.forEach((item) => {
+                            let resultText = "<span style='";
+                            if (typeof item.bold !== "undefined" && item.bold === true) {
+                                resultText += "font-weight:bold;";
+                            }
+                            if (typeof item.foreground !== "undefined" && item.bold === true) {
+                                resultText += "color:" + item.foreground + ";";
+                            }
+                            resultText += "'>" + item.text + "</span>";
+                            joinedLine += resultText;
+                        });
+                        html_text += joinedLine + "<br>";
+                    } else {
+                        html_text += parsedText[0].text + "<br>";
+                    }
+
+                    let matches;
+                    while ((matches = this.timeStampRegexp.exec(html_text)) !== null) {
+                        let startIndex = this.timeStampRegexp.lastIndex - matches[0].length;
+                        let endIndex = this.timeStampRegexp.lastIndex;
+
+                        let cutTimestamp = html_text.substring(startIndex, endIndex);
+                        let resTimestamp = "<span style='color: var(--bg-dark-accent-lighter);'>" + cutTimestamp + "</span>";
+                        html_text = resTimestamp + html_text.substring(endIndex);
+                    }
+                    consoleTextElem.innerHTML += html_text;
+                });
+
+                let scrollHeight = consoleTextElem.scrollHeight - Math.round(consoleTextElem.clientHeight) - 24;
+                if (!this.isItFirstLogRefresh) {
+                    this.isItFirstLogRefresh = true;
+                    consoleTextElem.scrollTop = scrollHeight;
+                } else {
+                    if ((scrollHeight - consoleTextElem.scrollTop) < 200) {
+                        consoleTextElem.scrollTop = scrollHeight;
+                    }
+                }
+            });
+        }
+    }
+
+    // Método para añadir el intervalo de refresco de la lista de tareas
+    addRefreshTasksInterval() {
+        this.addRefreshInterval(500, () => {
+            KubekTasksUI.refreshTasksList();
+        }, "tasksList");
+    }
+}
+
+// Definir el nuevo elemento personalizado
+customElements.define('kubek-refresher', KubekRefresherComponent);
+/* setTimeout(() => {
+    const kubekRefresher = document.querySelector('kubek-refresher');
+
+// Iniciar los intervalos de refresco
+kubekRefresher.addRefreshServerHeaderInterval();
+kubekRefresher.addRefreshServerLogInterval();
+kubekRefresher.addRefreshUsageInterval();
+kubekRefresher.addRefreshTasksInterval();
+}, 1000); */
--- web/pages/plugins.js
+++ web/pages/plugins.js
@@ -31,17 +31,36 @@ KubekPluginsUI = class {
     });
   }
 
-// Добавить мод/плагин в список
-  static addPluginItemToList(listElem, item, itemType){
-    let nameSplit = item.split('.');
-    let displayName = item.replaceAll(".jar", "").replaceAll(".dis", "");
-    let switchedOn = " checked";
-    let onclick1 = 'KubekPluginsUI.togglePluginOrMod("' + item + '", "' + itemType + '")';
-    let onclick2 = 'KubekFileManager.deleteFile("/' + itemType + 's/' + item + '", KubekPluginsUI.refreshAllLists)';
-    if(nameSplit[nameSplit.length - 1] === "dis"){
-      switchedOn = "";
-    }
-    let itemHTML = "<div class='item'><label class='switch'><input onchange='" + onclick1 + "' type='checkbox'" + switchedOn + "><span class='slider round'></span></label><span class='filename'>" + displayName + "</span><button class='dark-btn icon-only' onclick='" + onclick2 + "'><span class='material-symbols-rounded'>delete</span></button></div>";
+  static addPluginItemToList(listElem, item, itemType) {
+    // Preparar datos básicos
+    const nameSplit = item.split('.');
+    const displayName = item.replaceAll(".jar", "").replaceAll(".dis", "");
+    
+    // Construir rutas y acciones
+    const filePath = `/${itemType}s/${item}`;
+    console.log(filePath);
+    const onclick1 = ""
+    const onclick2 = "";
+/*     const onclick1 = `KubekPluginsUI.togglePluginOrMod("${item}", "${itemType}")`;
+    const onclick2 = `KubekFileManager.deleteFile("${filePath}", KubekPluginsUI.refreshAllLists)`; */
+    
+    // Determinar estado del switch
+    const switchedOn = nameSplit[nameSplit.length - 1] !== "dis" ? " checked" : "";
+    
+    // Construir HTML usando template literal para mejor legibilidad
+    const itemHTML = `
+      <div class='item'>
+        <label class='switch'>
+          <input onchange='${onclick1}' type='checkbox'${switchedOn}>
+          <span class='slider round'></span>
+        </label>
+        <span class='filename'>${displayName}</span>
+        <button class='dark-btn icon-only' onclick='${onclick2}'>
+          <span class='material-symbols-rounded'>delete</span>
+        </button>
+      </div>
+    `;
+    
     listElem.append(itemHTML);
   }
 
@@ -81,3 +100,238 @@ KubekPluginsUI = class {
     });
   }
 }
+let modsandplugins = {
+  plugins: [],
+  mods: []
+}
+async function getpluginsandmods(type) {
+  if(type === "plugins"){
+    KubekPlugins.getPluginsList((plugins) => {
+      modsandplugins.plugins = plugins;
+      setpluginsandmods(type);
+    });
+  } else if(type === "mods"){
+    KubekPlugins.getModsList((mods) => {
+      modsandplugins.mods = mods;
+      setpluginsandmods(type);
+    });
+  }
+  return modsandplugins;
+}
+class KubekPluginsUIclass extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this._elementList = [];
+    this.render();
+    this.setupEventListeners();
+    this.type = "plugins";
+  }
+
+  get elements() {
+    return this._elementList;
+  }
+
+  set elements(list) {
+    this._elementList = list;
+    this.renderList('elements-list', list, this.type);
+  }
+  connectedCallback() {
+    this.renderAllLists();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+      <style>
+
+        .item {
+          display: flex;
+          align-items: center;
+          margin-bottom: 10px;
+        }
+        .switch {
+          position: relative;
+          display: inline-block;
+          width: 60px;
+          height: 28px;
+        }
+        .switch input {
+          opacity: 0;
+          width: 0;
+          height: 0;
+        }
+        .slider {
+          position: absolute;
+          cursor: pointer;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #e0e0e0;
+          transition: .3s;
+          border-radius: 34px;
+        }
+        .slider:before {
+          position: absolute;
+          content: "";
+          height: 20px;
+          width: 20px;
+          left: 4px;
+          bottom: 4px;
+          background-color: white;
+          transition: .3s;
+          border-radius: 50%;
+        }
+        input:checked + .slider {
+          background-color: #2196F3;
+        }
+        input:checked + .slider:before {
+          transform: translateX(32px);
+        }
+        .dark-btn {
+          background-color: #333;
+          color: white;
+          border: none;
+          padding: 5px 10px;
+          cursor: pointer;
+        }
+      </style>
+      <div id="elements-list"></div>
+    `;
+  }
+
+  setupEventListeners() {
+    this.shadowRoot.addEventListener('change', this.handleToggle.bind(this));
+    this.shadowRoot.addEventListener('click', this.handleDelete.bind(this));
+  }
+
+  createItemHTML(item, itemType) {
+    const isEnabled = !item.endsWith('.dis');
+    const displayName = item.replace('.jar', '').replace('.dis', '');
+    console.log(displayName);
+    return `
+      <div class="item">
+        <label class="switch">
+          <input 
+            type="checkbox" 
+            ${isEnabled ? 'checked' : ''} 
+            data-item="${item}" 
+            data-type="${itemType}"
+          >
+          <span class="slider round"></span>
+        </label>
+        <span class="filename">${displayName}</span>
+        <button 
+          class="dark-btn icon-only" 
+          data-item="${item}" 
+          data-type="${itemType}"
+        >
+         <span class="material-symbols-outlined">
+        delete
+        </span>
+        </button>
+      </div>
+    `;
+  }
+
+  renderList(containerId, items, type) {
+    const container = this.shadowRoot.getElementById(containerId);
+    const html = items.map(item => this.createItemHTML(item, type)).join('');
+    container.innerHTML = html;
+  }
+
+  handleToggle(event) {
+    if (!event.target.matches('input[type="checkbox"]')) return;
+
+    const target = event.target;
+    const item = target.dataset.item;
+    const type = target.dataset.type;
+    const isEnabled = target.checked;
+    
+    const newName = isEnabled ? 
+      item.replace('.dis', '') : 
+      `${item}${item.endsWith('.dis') ? '' : '.dis'}`;
+
+    this.emitEvent('toggle', { item, type, newName });
+  }
+
+  handleDelete(event) {
+    const button = event.target.closest('button.dark-btn');
+    if (!button) return;
+    
+    this.emitEvent('delete', {
+      item: button.dataset.item,
+      type: button.dataset.type
+    });
+  }
+
+  emitEvent(eventName, detail) {
+    this.dispatchEvent(new CustomEvent(eventName, {
+      detail,
+      bubbles: true,
+      composed: true
+    }));
+  }
+
+  renderAllLists(type = "plugins") {
+    this.renderList('elements-list', this.elements, type);
+    this.setType(type);
+  }
+
+  setElementList(list) {
+    this.elements = list;
+  }
+  setType(type){
+    this.type = type;
+  }
+}
+
+customElements.define('kubek-plugins-ui', KubekPluginsUIclass);
+
+// Usage example:
+const kubekPluginsUI = document.querySelector('kubek-plugins-ui');
+
+['toggle', 'delete'].forEach(eventName => {
+  kubekPluginsUI.addEventListener(eventName, event => {
+    const detail = event.detail;
+    console.log(detail);
+    const itemname = detail.item;
+    if (eventName === "delete") {
+      deletefile(itemname, detail.type);
+    } else if (eventName === "toggle") {
+      togglepluginormod(itemname, detail.type, detail.newName);
+    }
+  });
+});
+kubekPluginsUI.renderAllLists("plugins");
+
+function setpluginsandmods(type) {
+  if(type === "plugins"){
+    kubekPluginsUI.setElementList(modsandplugins.plugins);
+  } else if(type === "mods"){
+    kubekPluginsUI.setElementList(modsandplugins.mods);
+  }
+}
+function deletefile(item, itemType){
+  if (itemType !== "plugins" && itemType !== "mods") {
+    console.log("Invalid item type", itemType);
+    return;
+  }
+// after refreshing the list
+  const itemtodelete = '/'+ itemType + '/'+ item;
+  console.log(itemtodelete);
+  // KubekFileManager.deleteFile("${filePath}", KubekPluginsUI.refreshAllLists) 1 params filePath string and callback function
+}
+function togglepluginormod(item, itemType, newName) {
+  if (itemType !== "plugins" && itemType !== "mods") {
+    console.log("Invalid item type", itemType);
+    return;
+  }
+  if (newName) {
+    const filePath = `/${itemType}/${item}`;
+    // KubekFileManager.renameFile(filePath, newName, KubekPluginsUI.refreshAllLists) 3 params filePath string, newName string and callback function
+    console.log(item, itemType, newName, filePath);
+  }
+}
+getpluginsandmods("plugins");
