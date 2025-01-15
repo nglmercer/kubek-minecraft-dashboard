411261861208d21d25264efc38c23a9a24e3f784
melserngl <128845117+nglmercer@users.noreply.github.com>
2025-01-14 19:23:07 -0500

    add webcomponent usage ram and cpu

--- web/pages/console.js
+++ web/pages/console.js
@@ -1,23 +1,37 @@
-consoleCpuUsageBar = new KubekCircleProgress($("#cpu-usage-bar"), 100, 100, "var(--bg-dark-accent)", "var(--bg-dark-accent-light)", "var(--bg-primary-500)");
-consoleCpuUsageBar.create();
-consoleRamUsageBar = new KubekCircleProgress($("#ram-usage-bar"), 100, 100, "var(--bg-dark-accent)", "var(--bg-dark-accent-light)", "var(--bg-primary-500)");
-consoleRamUsageBar.create();
-$("#ram-usage-bar").hide();
-$("#cpu-usage-bar").hide();
+const cpuElem = document.getElementById("cpu-bar");
+cpuElem.setAttribute("value", 55);
+cpuElem.setAttribute("active-color", "#007bff");
+const ramElem = document.getElementById("ram-bar");
+ramElem.setAttribute("value", 55);
+ramElem.setAttribute("active-color", "#007bff");
 
+function changeProgressvalue(element, value, action = "setvalue") {
+    const elements = {
+        "cpu": cpuElem,
+        "ram": ramElem
+    };
+    const actions = {
+        "setvalue": "value",
+        "setactivecolor": "active-color"
+    }
+    if (typeof elements[element] !== "undefined") {
+        if (typeof actions[action] !== "undefined") {
+            elements[element].setAttribute(actions[action], value);
+        }
+    }
+}
 KubekConsoleUI = class {
     // Обновить progress бары использования рес-ов
     static refreshUsageItems(cpu, ram, ramElem) {
-        consoleCpuUsageBar.setValue(cpu);
-        consoleRamUsageBar.setValue(ram);
-        consoleCpuUsageBar.setActiveColor(KubekUtils.getProgressGradientColor(cpu));
-        consoleRamUsageBar.setActiveColor(KubekUtils.getProgressGradientColor(ram));
+        changeProgressvalue("cpu", cpu);
+        changeProgressvalue("ram", ram);
+        changeProgressvalue("ram", KubekUtils.getProgressGradientColor(ram), "setactivecolor");
+        changeProgressvalue("cpu", KubekUtils.getProgressGradientColor(cpu), "setactivecolor");
+
         $("#ram-usage-text").text(KubekUtils.humanizeFileSize(ramElem.used) + " / " + KubekUtils.humanizeFileSize(ramElem.total));
         if ($("#cpu-usage-bar").css("display") === "none") {
             $("#cpu-usage-spinner").hide();
             $("#ram-usage-spinner").hide();
-            $("#ram-usage-bar").show();
-            $("#cpu-usage-bar").show();
         }
     }
 }
